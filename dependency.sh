2b0cfd72cb50928b199a874eff89e5ae48606b92
WANG Liang <redaready.dev@gmail.com>
2022-08-31 11:30:44 +0900

    deps for dev

---
+++ dependency.sh
@@ -0,0 +1,11 @@
+git clone -b v1.0.2 https://github.com/awslabs/aws-lc.git aws-lc #
+git clone -b v1.3.20 https://github.com/aws/s2n-tls.git
+git clone -b v0.8.0 https://github.com/awslabs/aws-c-common.git
+git clone -b v0.1.2 https://github.com/awslabs/aws-c-sdkutils.git
+git clone -b v0.5.18 https://github.com/awslabs/aws-c-cal.git
+git clone -b v0.11.0 https://github.com/awslabs/aws-c-io.git
+git clone -b v0.2.14 http://github.com/awslabs/aws-c-compression.git
+git clone -b v0.6.19 https://github.com/awslabs/aws-c-http.git
+git clone -b v0.6.15 https://github.com/awslabs/aws-c-auth.git
+git clone -b json-c-0.16-20220414 https://github.com/json-c/json-c.git
+git clone -b v0.2.1 https://github.com/aws/aws-nitro-enclaves-nsm-api.git
